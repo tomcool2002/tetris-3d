28efc1c8349ba5f8d0385f861bfc9529302d1ce0
Julian Angel Murillo <juliandelapaz2001@gmail.com>
2022-09-21 16:00:12 -0400

    f

--- camera.js
+++ camera.js
@@ -1,14 +1,21 @@
 // tourner la camera
+import "./main";
 
-controls = new OrbitControls(camera,renderer.domElement);
-camera.lookAt(0, 0, 0);
-camera.position.set(0, 0, 100);
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    // camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-  
-  window.addEventListener("resize", onWindowResize, false);
+function camera(){
+    controls = new OrbitControls(camera,renderer.domElement);
+    camera.lookAt(0, 0, 0);
+    camera.position.set(0, 0, 100);
+    
+    
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+      
+    window.addEventListener("resize", onWindowResize, false);
+}
+
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 
 let scene, camera, renderer, cube, line, geometry, material, controls;
 
