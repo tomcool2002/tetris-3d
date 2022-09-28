975d09e422a36309d069614bcc0cb9f7e809a3d6
tomcool2002 <32969040+tomcool2002@users.noreply.github.com>
2022-09-28 08:04:51 -0400

    random

--- piece.js
+++ piece.js
@@ -32,11 +32,11 @@ export class Piece extends THREE.Group {
 
   createRandomPiece() {
     //random number 1-7
-    let rand = 5 //Math.floor(Math.random() * 7) + 1;
+    let rand = Math.floor(Math.random() * 7) + 1;
     let piece = new THREE.Group();
     let color = this.getRandomColour();
 
-    console.log(rand);
+    //console.log(rand);
     switch (rand) {
       case 1: // I Shape
 
